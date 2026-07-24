eb41b65bf147b5efb1217d230ffc5820a47198f5
Cric2 <118618642+Chrismeeuwenoord@users.noreply.github.com>
2026-07-24 13:30:05 +0200

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -88,7 +88,7 @@ export default {
                         <p class="type-label-md">Original List by <a href="https://me.redlimerl.com/" target="_blank">RedLime</a></p>
                     </div>
                     <template v-if="editors">
-                        <h3>HCL Staff</h3>
+                        <h3>DSL Members</h3>
                         <ol class="editors">
                             <li v-for="editor in editors">
                                 <img :src="\`/assets/\${roleIconMap[editor.role]}\${store.dark ? '-dark' : ''}.svg\`" :alt="editor.role">
